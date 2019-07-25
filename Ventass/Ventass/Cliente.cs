5fe6f8b676b6d054a60448676328d1e7d4b90362
Luis <Luis@DESKTOP-6R5MDTE>
2019-07-25 16:36:03 -0600

    Se agrego clse Cliente y clase Ciudad

---
+++ Ventass/Ventass/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventass
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Ciudad { get; set; }
+    }
+}
---
+++ Ventass/Ventass/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ventass
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cliente1 = new Cliente();
+            cliente1.Id = 1;
+            cliente1.Nombre = "Luis Lopez";
+            cliente1.Telefono = 98745621;
+            cliente1.Ciudad = "San Pedro Sula";
+
+            var cliente2 = new Cliente();
+            cliente2.Id = 1;
+            cliente2.Nombre = "Carlos Lopez";
+            cliente2.Telefono = 96582314;
+            cliente2.Ciudad = "Choloma";
+
+            var listadeProductos = new List<Cliente>();
+            listadeProductos.Add(cliente1);
+            listadeProductos.Add(cliente2);
+
+            foreach (var cliente in listadeProductos)
+            {
+                MessageBox.Show(cliente.Nombre + " " + cliente.Ciudad);
+            }
+        }
+     }
+  }
